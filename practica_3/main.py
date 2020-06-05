64b3ddde9bc1352d7d1069c0a2cdd4637cb92731
Tarnomax <maxitarnovski@gmail.com>
2020-06-05 11:01:04 -0300

    actualizo main

--- practica_3/main.py
+++ practica_3/main.py
@@ -21,14 +21,12 @@ if __name__ == '__main__':
 
     ##  create a program
 
-    prg = Program("test.exe", [ASM.CPU(2), ASM.IO(), ASM.CPU(3), ASM.IO(), ASM.CPU(3)])
     prg1 = Program("prg1.exe", [ASM.CPU(2), ASM.IO(), ASM.CPU(3), ASM.IO(), ASM.CPU(2)])
     prg2 = Program("prg2.exe", [ASM.CPU(4), ASM.IO(), ASM.CPU(1)])
     prg3 = Program("prg3.exe", [ASM.CPU(3)])
 
     
     # execute the program
-    kernel.run(prg)
     kernel.run(prg1)
     kernel.run(prg2)
     kernel.run(prg3)
